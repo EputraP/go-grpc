8f66e811f0444cadce0b9393196efac93d6217cf
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-03-24 21:08:54 +0700

    add getproducts with dummy data

--- cmd/services/product.go
+++ cmd/services/product.go
@@ -1,9 +1,44 @@
 package services
 
 import (
+	"context"
+	pagingPb "go-grpc/pb/pagination"
 	productPb "go-grpc/pb/product"
 )
 
 type ProductService struct {
 	productPb.UnimplementedProductServiceServer
+}
+
+func (p *ProductService) GetProducts(context.Context, *productPb.Empty) (*productPb.Products, error) {
+	products := &productPb.Products{
+		Pagination: &pagingPb.Pagination{
+			Total:       10,
+			PerPage:     5,
+			CurrentPage: 1,
+			LastPage:    2,
+		},
+		Data: []*productPb.Product{
+			{
+				Id:    1,
+				Name:  "Metallica T-Shirt",
+				Price: 10000,
+				Stock: 10,
+				Category: &productPb.Category{
+					Id:   1,
+					Name: "Shirt",
+				},
+			}, {
+				Id:    2,
+				Name:  "Wibu T-Shirt",
+				Price: 20000,
+				Stock: 20,
+				Category: &productPb.Category{
+					Id:   1,
+					Name: "Shirt",
+				},
+			},
+		},
+	}
+	return products, nil
 }
