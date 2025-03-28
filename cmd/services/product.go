1baaf4bde5c474a16089d8f705e3ebc7b3b8c42a
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-03-29 03:41:42 +0700

    add  getproduct

--- cmd/services/product.go
+++ cmd/services/product.go
@@ -2,7 +2,8 @@ package services
 
 import (
 	"context"
-	"go-grpc/pb/pagination"
+	"go-grpc/cmd/helpers"
+	pagingPb "go-grpc/pb/pagination"
 	productPb "go-grpc/pb/product"
 	"log"
 
@@ -16,17 +17,27 @@ type ProductService struct {
 	DB *gorm.DB
 }
 
-func (p *ProductService) GetProducts(context.Context, *productPb.Empty) (*productPb.Products, error) {
+func (p *ProductService) GetProducts(ctx context.Context, pageParam *productPb.Page) (*productPb.Products, error) {
 	var products []*productPb.Product
+	var pagination pagingPb.Pagination
 
-	rows, err := p.DB.Table("products as p").
+	var page int64 = 1
+	if pageParam.GetPage() != 0 {
+		page = pageParam.GetPage()
+	}
+
+	sql := p.DB.Table("products as p").
 		Joins("LEFT JOIN Categories AS c ON c.id = p.category_id").
-		Select("p.id", "p.name", "p.price", "p.stock", "c.id as category_id", "c.name as category_name").
-		Rows()
+		Select("p.id", "p.name", "p.price", "p.stock", "c.id as category_id", "c.name as category_name")
+
+	offset, limit := helpers.Pagination(sql, page, &pagination)
+
+	rows, err := sql.Offset(int(offset)).Limit(int(limit)).Rows()
 
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
+
 	defer rows.Close()
 
 	for rows.Next() {
@@ -41,14 +52,26 @@ func (p *ProductService) GetProducts(context.Context, *productPb.Empty) (*produc
 	}
 
 	response := &productPb.Products{
-		Pagination: &pagination.Pagination{
-			Total:       2,
-			PerPage:     1,
-			CurrentPage: 1,
-			LastPage:    1,
-		},
-		Data: products,
+		Pagination: &pagination,
+		Data:       products,
 	}
 
 	return response, nil
+}
+
+func (p *ProductService) GetProduct(ctx context.Context, id *productPb.Id) (*productPb.Product, error) {
+	row := p.DB.Table("products as p").
+		Joins("LEFT JOIN Categories AS c ON c.id = p.category_id").
+		Select("p.id", "p.name", "p.price", "p.stock", "c.id as category_id", "c.name as category_name").
+		Where("p.id = ?", id.GetId()).Row()
+
+	var product productPb.Product
+	var category productPb.Category
+
+	if err := row.Scan(&product.Id, &product.Name, &product.Price, &product.Stock, &category.Id, &category.Name); err != nil {
+		log.Fatal(err)
+	}
+	product.Category = &category
+
+	return &product, nil
 }
